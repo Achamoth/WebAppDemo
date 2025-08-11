a65dd5076b72b399b6e5708598468ddac1e69624
Ammar Abu Shamleh <ammar.abushamleh@interfuze.com.au>
2025-08-11 15:53:11 +0800

    Add nuget example

---
+++ DemoLib/DemoLib/Utility.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoLib
+{
+    public static class Utility
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
